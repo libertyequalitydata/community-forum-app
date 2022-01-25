083ef8648cdcc34fc47c1b3774ee8441c0d31aa7
hamza <hamzahalilovic.12@gmail.com>
2022-01-25 16:18:48 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Text, Button, ChakraProvider } from "@chakra-ui/react";
+
+import { ChakraProvider, Text } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
         <Text>App</Text>
-        <Button>dsasda</Button>
-        <Button colorScheme="teal" size="xs">
-          Button
-        </Button>
       </div>
     </ChakraProvider>
   );
