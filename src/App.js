c82fa30491e7290e0183a80c1a4cab30bac84b6e
hamza <hamzahalilovic.12@gmail.com>
2022-01-25 16:16:58 +0100

    storybook setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Text, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Text>App</Text>
+        <Button>dsasda</Button>
+        <Button colorScheme="teal" size="xs">
+          Button
+        </Button>
+      </div>
+    </ChakraProvider>
   );
 }
 
